b355b17c0019a6e10950a5d8a461ecc017b0e0ed
LeonardNolting <49598438+LeonardNolting@users.noreply.github.com>
2020-11-02 16:03:27 +0100

    Basic test

--- bot.ts
+++ bot.ts
@@ -2,6 +2,10 @@ const config = require("./config"),
 	Discord = require("discord.js"),
 	client = new Discord.Client()
 
+client.on("message", async msg => {
+	if (msg.content.startsWith("~") && msg.content.length > 1) msg.reply("You executed the command " + msg.content.substr(1))
+})
+
 client.on("ready", async () => {
 	console.log("ready")
 })
